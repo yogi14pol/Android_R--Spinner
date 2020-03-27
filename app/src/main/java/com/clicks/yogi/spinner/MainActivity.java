27aa9e04d1e609f8582c6314111e0bb317548b7c
YOGESH POL <yogeshpol.2016@gmail.com>
2020-03-27 13:31:43 +0530

    Spinner with SetOnItemSelectedListener method is added to view the spinner item name...

--- app/src/main/java/com/clicks/yogi/spinner/MainActivity.java
+++ app/src/main/java/com/clicks/yogi/spinner/MainActivity.java
@@ -1,10 +1,13 @@
 package com.clicks.yogi.spinner;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +24,54 @@ public class MainActivity extends AppCompatActivity {
     arrayAdapter = new ArrayAdapter<>(MainActivity.this,android.R.layout.simple_spinner_dropdown_item,ipl);
     spin.setAdapter(arrayAdapter);
 
+        spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+/*
+            // Type 1: By this code you can retrieve the selected item from the dropdown item...
+            Toast.makeText(MainActivity.this, "Team: "+parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+*/
+                // Type 2:
+                switch (position) {
+
+                    case 0:
+                        Toast.makeText(MainActivity.this, "Hey CSK", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1:
+                        Toast.makeText(MainActivity.this, "Hey KKR", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 2:
+                        Toast.makeText(MainActivity.this, "Hey RCB", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 3:
+                        Toast.makeText(MainActivity.this, "Hey DC", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 4:
+                        Toast.makeText(MainActivity.this, "Hey MI", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 5:
+                        Toast.makeText(MainActivity.this, "Hey RR", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 6:
+                        Toast.makeText(MainActivity.this, "Hey KXI-PUNJAB", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 7:
+                        Toast.makeText(MainActivity.this, "Hey SH", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 8:
+                        Toast.makeText(MainActivity.this, "Hey DD", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        Toast.makeText(MainActivity.this, "Something went wrong...", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // This method is called whenever item is removed from the list...
+            }
+        });
+    
     }
 }
